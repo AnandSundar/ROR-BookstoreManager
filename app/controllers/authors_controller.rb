21145c4e83bf323edd59e5a34f09d74f9da3da04
AnandSundar <anandsundar96@gmail.com>
2017-11-26 22:40:48 -0800

    finished implementing all controllers,  models and views. website fully operational

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,21 +5,35 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    @author = Author.new(author_params)
+    if @author.save
+      flash[:notice] = "Author Created"
+      redirect_to authors_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @author = Author.find(params[:id])
+    @author.update(author_params)
+    flash[:notice] = "Author Updated"
+    redirect_to authors_path
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
   def destroy
+    @author = Author.find(params[:id])
+    @author.destroy
+    flash[:notice] = "Author Removed"
+    redirect_to authors_path
   end
 
   def index
-  end
-
-  def show
+    @authors = Author.all
   end
 
   private
--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -5,21 +5,36 @@ class PublishersController < ApplicationController
   end
 
   def create
+
+    @publisher = Publisher.new(publisher_params)
+    if @publisher.save
+      flash[:notice] = "Publisher Created"
+      redirect_to publishers_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @publisher = Publisher.find(params[:id])
+    @publisher.update(publisher_params)
+    flash[:notice] = "Publisher Updated"
+    redirect_to publishers_path
   end
 
   def edit
+    @publisher = Publisher.find(params[:id])
   end
 
   def destroy
+    @publisher = Publisher.find(params[:id])
+    @publisher.destroy
+    flash[:notice] = "Publisher Removed"
+    redirect_to publishers_path
   end
 
   def index
-  end
-
-  def show
+    @publishers = Publisher.all
   end
 
   private
