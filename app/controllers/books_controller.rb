cb41a03d349431723ff48a3dcdd8e01665c74aaa
AnandSundar <anandsundar96@gmail.com>
2017-11-27 09:37:34 -0800

    fixed syntax error in book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,9 @@ class BooksController < ApplicationController
     @category = Category.new
     @author = Author.new
     @publisher = Publisher.new
-
   end
 
   def create
-
     @book = Book.new(book_params)
     @book.save
 
@@ -47,6 +45,6 @@ class BooksController < ApplicationController
 
   private
     def book_params
-    params.require(:book).permit(:title, :category_id, :author_id, :publisher_id, :isbn, :price, :buy, :format, :excerpt, :pages, :year, :coverpath)
-  end
+      params.require(:book).permit(:title, :category_id, :author_id, :publisher_id, :isbn, :price, :buy, :format, :excerpt, :pages, :year, :coverpath)
+    end
 end
